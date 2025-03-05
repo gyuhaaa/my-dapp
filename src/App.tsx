0d7e87b35d4f7a5d03f3bd8c4ca7f79445480b38
gyuhaaa <vkvkvk2892@naver.com>
2025-03-05 09:50:45 +0900

    ing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ethers, JsonRpcSigner } from "ethers";
+import ContractABI from "./ContractABI.json";
+import { Contract } from "ethers";
 
 export default function App() {
   const [signer, setSigner] = useState<JsonRpcSigner | null>(null);
+  const [contract, setContract] = useState<Contract | null>(null);
+  const [totalSupply, setTotalSupply] = useState<number>(0);
 
   const connectMetamask = async () => {
     try {
@@ -19,6 +23,31 @@ export default function App() {
     }
   };
 
+  const fetchTotalSupply = async () => {
+    if (!contract) return;
+
+    try {
+      const res = await contract.totalSupply();
+
+      setTotalSupply(Number(res));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    if (!signer) return;
+
+    setContract(
+      new ethers.Contract(
+        import.meta.env.VITE_CONTRACT_ADDRESS,
+        ContractABI,
+        signer
+      )
+    );
+    console.log(contract);
+  }, [signer]);
+
   return (
     <div style={{ padding: "2rem" }}>
       <h1>React & Vite + MetaMask 연결 예제</h1>
@@ -29,6 +58,8 @@ export default function App() {
             {signer.address.substring(signer.address.length - 5)}
           </p>
           <button onClick={() => setSigner(null)}>Disconnect</button>
+          {totalSupply && <p>총 공급량: {totalSupply}</p>}
+          <button onClick={fetchTotalSupply}>토큰 총 공급량 조회</button>
         </div>
       ) : (
         <button onClick={connectMetamask}>Connect Metamask</button>
